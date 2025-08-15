fbeee800bbff02937db9178acc22323701773d5b
OmriTafla <omritafla@gmail.com>
2025-08-15 14:12:57 +0300

    connector angle tweening

--- Assets/Scripts/ConnectorVisualizer.cs
+++ Assets/Scripts/ConnectorVisualizer.cs
@@ -1,9 +1,29 @@
+using System.Collections;
 using UnityEngine;
 
 public class ConnectorVisualizer : MonoBehaviour
 {
+    [SerializeField] private float turningTime;
+
     public void ChangeAngle(float angle)
     {
-        transform.rotation = Quaternion.Euler(0, 0, angle);
+        StopAllCoroutines();
+        StartCoroutine(Rotate(angle));
+    }
+
+    private IEnumerator Rotate(float targetAngle)
+    {
+        float currentAngle = transform.eulerAngles.z;
+        float startTime = Time.time;
+
+        while (Time.time - startTime < turningTime)
+        {
+            float t = (Time.time - startTime) / turningTime;
+            float angle = Mathf.LerpAngle(currentAngle, targetAngle, t);
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+            yield return null;
+        }
+
+        transform.rotation = Quaternion.Euler(0, 0, targetAngle);
     }
 }
